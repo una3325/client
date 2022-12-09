c72984ef3df87a24035469eb8e18e5c55303e493
Yang yuna <a33256448@gmail.com>
2022-12-09 18:07:33 +0900

    2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './App.css'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 function App() {
+  const [listOfUsers, setListOfUsers] = useState([])
+  const [name, setName] = useState('')
+  const [age, setAge] = useState(0)
+  const [username, setUsername] = useState('')
+  useEffect(() => {
+    axios.get('http://localhost:3001/getUsers').then((response) => {
+      setListOfUsers(response.data)
+    })
+  }, [])
+  const createUser = () => {
+    axios
+      .post('http://localhost:3001/createUser', { name, age, username })
+      .then((response) => {
+        alert('User Created!')
+        setListOfUsers([...listOfUsers, { name, age, username }])
+      })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      <div>
+        {listOfUsers.map((user) => {
+          return (
+            <div className="list">
+              <h3>
+                Name: {user.name}, Age: {user.age}, Username: {user.username}
+              </h3>
+            </div>
+          )
+        })}
+      </div>
 
-export default App;
+      <div>
+        <input
+          type="text"
+          placeholder="Name"
+          onChange={(event) => setName(event.target.value)}
+        />
+        <input
+          type="number"
+          placeholder="Age"
+          onChange={(event) => setAge(event.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Username"
+          onChange={(event) => setUsername(event.target.value)}
+        />
+        <button onClick={createUser}>Create User</button>
+      </div>
+    </div>
+  )
+}
+export default App
